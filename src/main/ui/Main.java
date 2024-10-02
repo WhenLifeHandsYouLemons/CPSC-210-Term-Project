be56842d892fc4585a945dac447cd2cc999c63de
Sooraj Sannabhadti <ssannabh@students.cs.ubc.ca>
2024-10-02 10:29:55 -0700

    Created new class files for project
    Added new files for the model and the tests for those files.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,10 @@
 package ui;
 
+import model.Library;
+import model.Book;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project!");
+        Library library = new Library();
     }
 }
