a5b62ce4809495b07f123b5812d12e7f2412f9c2
Sooraj Sannabhadti <ssannabh@students.cs.ubc.ca>
2024-10-11 09:31:11 -0700

    Added tests for new method stubs

--- src/test/ui/TestBookTrackerApp.java
+++ src/test/ui/TestBookTrackerApp.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.Book;
 import model.Library;
 
 public class TestBookTrackerApp {
@@ -52,5 +53,59 @@ public class TestBookTrackerApp {
 
         assertEquals("Library 1", testBookTrackerApp.findLibrary("Library 1").getLibraryName());
         assertEquals("Library 2", testBookTrackerApp.findLibrary("Library 2").getLibraryName());
+    }
+
+    @Test
+    void testAveragePageCount() {
+        testBookTrackerApp.addLibrary("Library 1");
+        testBookTrackerApp.addLibrary("Library 2");
+
+        Library library1 = testBookTrackerApp.findLibrary("Library 1");
+        Book testBook1 = new Book("Book 1", 100, 60);
+        library1.addBookToHistory(testBook1);
+
+        Library library2 = testBookTrackerApp.findLibrary("Library 2");
+        Book testBook2 = new Book("Book 2", 250, 100);
+        Book testBook3 = new Book("Book 3", 230, 120);
+        library2.addBookToHistory(testBook2);
+        library2.addBookToHistory(testBook3);
+
+        assertEquals(193.33, testBookTrackerApp.getAveragePageCount());
+    }
+
+    @Test
+    void testAverageWordCount() {
+        testBookTrackerApp.addLibrary("Library 1");
+        testBookTrackerApp.addLibrary("Library 2");
+
+        Library library1 = testBookTrackerApp.findLibrary("Library 1");
+        Book testBook1 = new Book("Book 1", 100, 60);
+        library1.addBookToHistory(testBook1);
+
+        Library library2 = testBookTrackerApp.findLibrary("Library 2");
+        Book testBook2 = new Book("Book 2", 250, 100);
+        Book testBook3 = new Book("Book 3", 230, 120);
+        library2.addBookToHistory(testBook2);
+        library2.addBookToHistory(testBook3);
+
+        assertEquals(0.0, testBookTrackerApp.getAverageWordCount());
+    }
+
+    @Test
+    void testAverageReadingDuration() {
+        testBookTrackerApp.addLibrary("Library 1");
+        testBookTrackerApp.addLibrary("Library 2");
+
+        Library library1 = testBookTrackerApp.findLibrary("Library 1");
+        Book testBook1 = new Book("Book 1", 100, 60);
+        library1.addBookToHistory(testBook1);
+
+        Library library2 = testBookTrackerApp.findLibrary("Library 2");
+        Book testBook2 = new Book("Book 2", 250, 100);
+        Book testBook3 = new Book("Book 3", 230, 120);
+        library2.addBookToHistory(testBook2);
+        library2.addBookToHistory(testBook3);
+
+        assertEquals(93.33, testBookTrackerApp.getAverageDuration());
     }
 }
