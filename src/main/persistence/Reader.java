6f63d481e4e5c461ed59e4c450d0ed6a96809b29
Sooraj Sannabhadti <ssannabh@students.cs.ubc.ca>
2024-10-22 11:49:57 -0700

    Added method stubs for Reader and Writer

--- src/main/persistence/Reader.java
+++ src/main/persistence/Reader.java
@@ -1,5 +1,16 @@
 package persistence;
 
 public class Reader {
+    // REQUIRES: filePath is not empty
+    // EFFECTS: Instantiates the Reader object and sets the read source path to the given string
+    public Reader(String filePath) {
+        // stub
+    }
 
+    // REQUIRES: filePath should not be empty
+    // MODIFIES: this
+    // EFFECTS: Runs functions to read the file at filePath and convert the JSON to a program state
+    public void readFromFile() {
+        // stub
+    }
 }
--- src/main/persistence/Writer.java
+++ src/main/persistence/Writer.java
@@ -1,5 +1,38 @@
 package persistence;
 
-public class Writer {
+import org.json.JSONObject;
+import java.io.*;
 
+import ui.BookTrackerApp;
+
+public class Writer {
+    // REQUIRES: filePath is not empty
+    // EFFECTS: Instantiates the Writer object and creates a new JSONObject for saving data
+    public Writer(String filePath) {
+        // stub
+    }
+
+    // REQUIRES: 
+    // MODIFIES: 
+    // EFFECTS: Runs functions to convert the current program state to JSON and save it to filePath
+    public void writeToFile(BookTrackerApp bookTrackerApp) {
+        // stub
+    }
+
+    // REQUIRES: JSONObject is not null
+    // MODIFIES: 
+    // EFFECTS: Converts the given jsonObject to a string
+    public String convertToString(JSONObject jsonObject) {
+        return "";  // stub
+    }
+
+    // REQUIRES: filePath is not empty
+    // EFFECTS: Saves the given stringToSave to the filePath
+    public void saveToFile(String stringToSave) {
+        // stub
+    }
+
+    public JSONObject getJsonObject() {
+        return null;    // stub
+    }
 }
