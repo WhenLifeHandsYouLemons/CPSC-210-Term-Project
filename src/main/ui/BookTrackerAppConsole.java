45a39c9bf9834d6b6bf265bdf7e9a6dd6ded316a
Sooraj Sannabhadti <ssannabh@students.cs.ubc.ca>
2024-10-29 12:19:17 -0700

    Created main GUI & moved console version from Main

---
+++ src/main/ui/BookTrackerAppConsole.java
@@ -0,0 +1,355 @@
+package ui;
+
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+import model.*;
+import persistence.*;
+
+public class BookTrackerAppConsole {
+    public BookTrackerAppConsole() {
+        // Create scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        String option = "";
+
+        // Create main application instance
+        BookTrackerApp bookTrackerApp = new BookTrackerApp();
+
+        // Create serialiser instances for save/load features
+        String filePath = "./data/bookTrackerAppData.json";
+        Writer writer = new Writer(filePath);
+        Reader reader = new Reader(filePath);
+
+        // Run main program
+        homePage(bookTrackerApp, scanner, option, writer, reader);
+
+        // Close scanner to stop memory leaks
+        scanner.close();
+    }
+
+    // MODIFIES: bookTrackerApp, option, writer, reader
+    // EFFECTS: Prints out available user options and asks for user input until
+    // "exit" is typed
+    private static void homePage(BookTrackerApp bookTrackerApp, Scanner scanner, String option, Writer writer,
+            Reader reader) {
+        do {
+            printHomeHelpMessage();
+
+            option = scanner.nextLine();
+            if (option.equals("new library")) {
+                newLibraryOption(bookTrackerApp, scanner);
+            } else if (option.equals("view library")) {
+                viewLibraryOption(bookTrackerApp, scanner);
+            } else if (option.equals("view stats")) {
+                viewStatisticsOption(bookTrackerApp);
+            } else if (option.equals("view all books")) {
+                viewAllBooksOption(bookTrackerApp);
+            } else if (option.equals("remove library")) {
+                removeLibraryOption(bookTrackerApp, scanner);
+            } else if (option.equals("save library")) {
+                saveLibraryOption(writer, bookTrackerApp);
+            } else if (option.equals("load library")) {
+                bookTrackerApp = loadLibraryOption(reader, bookTrackerApp);
+            }
+        } while (!option.equals("exit"));
+    }
+
+    // MODIFIES: writer
+    // EFFECTS: Saves all the data currently stored in bookTrackerApp to a JSON file
+    private static void saveLibraryOption(Writer writer, BookTrackerApp bookTrackerApp) {
+        try {
+            writer.writeToFile(bookTrackerApp);
+            System.out.println("Saved libraries to '" + writer.getFilePath() + "'!");
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to save libraries to '" + writer.getFilePath() + "'! Please try again.");
+        }
+    }
+
+    // MODIFIES: bookTrackerApp
+    // EFFECTS: Reads app data from a JSON file into the given bookTrackerApp
+    // instance
+    private static BookTrackerApp loadLibraryOption(Reader reader, BookTrackerApp bookTrackerApp) {
+        try {
+            bookTrackerApp = reader.readFromFile();
+            System.out.println("Loaded libraries from '" + reader.getFilePath() + "'!");
+        } catch (IOException e) {
+            System.out.println("Unable to load libraries from '" + reader.getFilePath() + "'! Please try again.");
+        }
+
+        return bookTrackerApp;
+    }
+
+    // EFFECTS: Prints out the options available to the user on the home page
+    private static void printHomeHelpMessage() {
+        System.out.println("\n----------------------------------------------------------");
+        System.out.println("Choose one of the following options:\n");
+        System.out.println("'new library' - Adds a new library");
+        System.out.println("'view library' - Opens a library");
+        System.out.println("'view stats' - Prints out your total reading statistics");
+        System.out.println("'view all books' - Prints out all the books you've added");
+        System.out.println("'remove library' - Removes a library");
+        System.out.println("'save library' - Saves all libraries and books tracked to an external file.");
+        System.out.println("'load library' - Loads libraries and books tracked from an external file.");
+        System.out.println("'exit' - Closes the program");
+        System.out.println("----------------------------------------------------------");
+    }
+
+    // EFFECTS: Prints out the options available to the user inside a library
+    private static void printLibraryHelpMessage(Library selectedLibrary) {
+        System.out.println("\n----------------------------------------------------------");
+        System.out.println(selectedLibrary.getName());
+        System.out.println("Choose one of the following options:\n");
+        System.out.print("'add book' - Adds a new book to ");
+        System.out.println(selectedLibrary.getName());
+        System.out.print("'view stats' - Prints out your reading statistics for ");
+        System.out.println(selectedLibrary.getName());
+        System.out.print("'view books' - Prints out the books you've added to ");
+        System.out.println(selectedLibrary.getName());
+        System.out.print("'remove book' - Removes a book from ");
+        System.out.println(selectedLibrary.getName());
+        System.out.println("'back' - Exits the library and goes back to the home page");
+        System.out.println("----------------------------------------------------------");
+    }
+
+    // MODIFIES: bookTrackerApp
+    // EFFECTS: Prints out all libraries available, asks for a library's name, and
+    // opens that library
+    private static void removeLibraryOption(BookTrackerApp bookTrackerApp, Scanner scanner) {
+        System.out.println("Libraries available:");
+        for (Library library : bookTrackerApp.getLibraries()) {
+            System.out.print("- ");
+            System.out.println(library.getName());
+        }
+
+        // Ask for library name input
+        System.out.print("Enter the name of the library to remove: ");
+        String libraryName = scanner.nextLine();
+
+        if (bookTrackerApp.findLibrary(libraryName) != null) {
+            // Confirm if want to remove
+            System.out.print("Are you sure you want to remove '");
+            System.out.print(libraryName);
+            System.out.println("'? (Enter 'yes' to delete)");
+
+            String confirm = scanner.nextLine();
+            if (confirm.equals("yes")) {
+                // Remove library
+                bookTrackerApp.removeLibrary(libraryName);
+                System.out.print("The library '");
+                System.out.print(libraryName);
+                System.out.println("' has been removed successfully!");
+            } else {
+                System.out.println("The library was not removed!");
+            }
+        } else {
+            System.out.println("The library you entered wasn't found!");
+        }
+    }
+
+    // MODIFIES: bookTrackerApp
+    // EFFECTS: Asks for the name of the new library and creates it
+    private static void newLibraryOption(BookTrackerApp bookTrackerApp, Scanner scanner) {
+        System.out.print("\nEnter the name of the library to be created: ");
+
+        // Ask for library name input
+        String name = scanner.nextLine();
+        if (!name.equals("")) {
+            bookTrackerApp.addLibrary(name);
+            System.out.print("Created library titled '");
+            System.out.print(name);
+            System.out.println("' successfully!");
+        }
+    }
+
+    // EFFECTS: Prints out information on all books that have been tracked
+    private static void viewAllBooksOption(BookTrackerApp bookTrackerApp) {
+        int index = 1;
+
+        System.out.println("\n------------------------------------------------");
+        System.out.println("All books:\n");
+        for (Library library : bookTrackerApp.getLibraries()) {
+            for (Book book : library.getBookCollection()) {
+                System.out.print(index);
+                System.out.print(". ");
+                System.out.println(book.getName());
+                System.out.print("    Page count: ");
+                System.out.println(book.getPageCount());
+                System.out.print("    Word count: ");
+                System.out.println(book.getWordCount());
+                System.out.print("    Reading duration (minutes): ");
+                System.out.println(book.getDuration());
+
+                index++;
+            }
+        }
+        System.out.println("\n------------------------------------------------");
+    }
+
+    // EFFECTS: Prints out overall statistics for all books
+    private static void viewStatisticsOption(BookTrackerApp bookTrackerApp) {
+        System.out.println("\n------------------------------------------------");
+        System.out.println("All Statistics:\n");
+        System.out.print("Average book length (in pages): ");
+        System.out.println(bookTrackerApp.getAveragePageCount());
+        System.out.print("Average book length (in words): ");
+        System.out.println(bookTrackerApp.getAverageWordCount());
+        System.out.print("Average reading duration (in minutes): ");
+        System.out.println(bookTrackerApp.getAverageDuration());
+        System.out.println("\n------------------------------------------------");
+    }
+
+    // EFFECTS: Prints out all libraries available, asks for the name of a library
+    // and opens it
+    private static void viewLibraryOption(BookTrackerApp bookTrackerApp, Scanner scanner) {
+        listAllLibraries(bookTrackerApp);
+
+        System.out.print("Enter the name of the library you want to open: ");
+        String option = scanner.nextLine();
+
+        if (bookTrackerApp.findLibrary(option) != null) {
+            Library selectedLibrary = bookTrackerApp.findLibrary(option);
+
+            System.out.print("Opened '");
+            System.out.print(selectedLibrary.getName());
+            System.out.println("'!");
+
+            listLibraryOptions(selectedLibrary, scanner);
+        } else {
+            System.out.println("Sorry! The library you've entered doesn't exist.");
+        }
+    }
+
+    // EFFECTS: Prints out all available libraries
+    private static void listAllLibraries(BookTrackerApp bookTrackerApp) {
+        System.out.println("\nLibraries available:");
+        List<Library> libraries = bookTrackerApp.getLibraries();
+        for (Library library : libraries) {
+            System.out.print("- ");
+            System.out.println(library.getName());
+        }
+        System.out.println("");
+    }
+
+    // MODIFIES: selectedLibrary
+    // EFFECTS: Asks for the information about a book and adds it to the selected
+    // library
+    private static void addBookOption(Library selectedLibrary, Scanner scanner) {
+        try {
+            System.out.print("Enter the name of the book: ");
+            String bookName = scanner.nextLine();
+            System.out.print("Enter the number of pages: ");
+            int pageCount = scanner.nextInt();
+            System.out.print("Enter the time taken to read the book (in whole minutes): ");
+            int readingDuration = scanner.nextInt();
+
+            if (bookName != "" && pageCount > 0 && readingDuration > 0) {
+                Book newBook = new Book(bookName, pageCount, readingDuration);
+                selectedLibrary.addBookToHistory(newBook);
+
+                System.out.print("Added '");
+                System.out.print(bookName);
+                System.out.println("' to the library successfully!");
+            } else {
+                System.out.println("Sorry! Couldn't create the book.");
+            }
+        } catch (Exception e) {
+            System.out.println("Sorry! Couldn't create the book and add it to library.");
+        }
+    }
+
+    // EFFECTS: Prints out the overall statistics of the selected library
+    private static void viewLibraryStatisticsOption(Library selectedLibrary) {
+        System.out.println("\n------------------------------------------------");
+        System.out.print(selectedLibrary.getName());
+        System.out.println(" Statistics:\n");
+        System.out.print("Average book length (in pages): ");
+        System.out.println(selectedLibrary.getAveragePageCount());
+        System.out.print("Average book length (in words): ");
+        System.out.println(selectedLibrary.getAverageWordCount());
+        System.out.print("Average reading duration (in minutes): ");
+        System.out.println(selectedLibrary.getAverageDuration());
+        System.out.println("\n------------------------------------------------");
+    }
+
+    // EFFECTS: Prints out information on all books in the selected library
+    private static void viewLibraryBooksOption(Library selectedLibrary) {
+        int index = 1;
+
+        System.out.println("\n------------------------------------------------");
+        System.out.print(selectedLibrary.getName());
+        System.out.println("'s books:\n");
+        for (Book book : selectedLibrary.getBookCollection()) {
+            System.out.print(index);
+            System.out.print(". ");
+            System.out.println(book.getName());
+            System.out.print("    Page count: ");
+            System.out.println(book.getPageCount());
+            System.out.print("    Word count: ");
+            System.out.println(book.getWordCount());
+            System.out.print("    Reading duration (minutes): ");
+            System.out.println(book.getDuration());
+
+            index++;
+        }
+        System.out.println("\n------------------------------------------------");
+    }
+
+    // MODIFIES: selectedLibrary
+    // EFFECTS: Prints out all books in the selected library, asks for a book name,
+    // and removes that book
+    private static void removeBookOption(Library selectedLibrary, Scanner scanner) {
+        System.out.println("Books available:");
+        for (Book book : selectedLibrary.getBookCollection()) {
+            System.out.print("- ");
+            System.out.println(book.getName());
+        }
+
+        System.out.print("Enter the name of the book to remove: ");
+        String bookName = scanner.nextLine();
+
+        if (selectedLibrary.findBook(bookName) != null) {
+            System.out.print("Are you sure you want to remove '");
+            System.out.print(bookName);
+            System.out.println("'? (Enter 'yes' to delete)");
+
+            String confirm = scanner.nextLine();
+            if (confirm.equals("yes")) {
+                selectedLibrary.removeBookFromLibrary(bookName);
+                System.out.print("The book '");
+                System.out.print(bookName);
+                System.out.println("' has been removed successfully!");
+            } else {
+                System.out.println("The book was not removed from the library!");
+            }
+        } else {
+            System.out.println("The book you entered wasn't found in this library!");
+        }
+    }
+
+    // Prints out available options in the library and asks for input on an option
+    private static void listLibraryOptions(Library selectedLibrary, Scanner scanner) {
+        String option;
+
+        do {
+            printLibraryHelpMessage(selectedLibrary);
+
+            option = scanner.nextLine();
+            switch (option) {
+                case "add book":
+                    addBookOption(selectedLibrary, scanner);
+                    break;
+                case "view stats":
+                    viewLibraryStatisticsOption(selectedLibrary);
+                    break;
+                case "view books":
+                    viewLibraryBooksOption(selectedLibrary);
+                    break;
+                case "remove book":
+                    removeBookOption(selectedLibrary, scanner);
+                    break;
+            }
+        } while (!option.equals("back"));
+    }
+}
--- src/main/ui/BookTrackerAppGUI.java
+++ src/main/ui/BookTrackerAppGUI.java
@@ -3,54 +3,237 @@ package ui;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Font;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.*;
 
-public class BookTrackerAppGUI {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Book Tracker App"); // Create the main app frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set the program to exit when the close button is
-                                                              // clicked
-        frame.setSize(850, 600); // Set window size
+import model.Library;
+import persistence.Reader;
+import persistence.Writer;
+
+public class BookTrackerAppGUI extends JFrame implements ActionListener {
+    protected BookTrackerApp bta;
+    private String filePath = "./data/bookTrackerAppData.json";
+
+    public BookTrackerAppGUI() {
+        super("Book Tracker App"); // Create the main app frame
+
+        bta = new BookTrackerApp();
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Set the program to exit when the close button is
+                                                           // clicked
+        setSize(850, 600); // Set window size
+
+        int columnPadding = 50;
 
         JPanel titlePanel = new JPanel();
         titlePanel.setBackground(new Color(255, 255, 255));
 
         JPanel mainContentPanel = new JPanel();
-        mainContentPanel.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
         mainContentPanel.setLayout(new GridLayout(1, 3));
-
-        int buttonWidth = 200;
-        int buttonHeight = 75;
 
         // Title section
         JLabel titleText = new JLabel("Book Tracker App");
-        titleText.setSize(frame.getWidth(), titleText.getMinimumSize().height);
+        titleText.setSize(getWidth(), titleText.getMinimumSize().height);
         titleText.setFont(new Font("SansSerif", Font.BOLD, 32));
         titlePanel.add(titleText);
 
         // First column section
-        JPanel firstColumnPanel = new JPanel();
-        firstColumnPanel.setLayout(null);
-        firstColumnPanel.setBackground(new Color(255, 0, 0));
+        JPanel firstColumn = new JPanel();
+        JPanel firstColumnInfo = new JPanel();
+        firstColumn
+                .setBorder(BorderFactory.createEmptyBorder(columnPadding, columnPadding, columnPadding, columnPadding));
+        firstColumn.setBackground(new Color(255, 0, 0));
+        firstColumnInfo.setBackground(null);
+        firstColumn.setLayout(new GridLayout(1, 1));
+        firstColumnInfo.setLayout(new GridLayout(3, 1, 0, 75));
 
-        // second column section
-        JPanel secondColumnPanel = new JPanel();
-        secondColumnPanel.setLayout(null);
-        secondColumnPanel.setBackground(new Color(0, 255, 0));
+        // New library button
+        JButton newLibraryButton = new JButton("New Library");
+        newLibraryButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        newLibraryButton.setBackground(new Color(255, 255, 0));
+        firstColumnInfo.add(newLibraryButton);
 
-        // third column section
-        JPanel thirdColumnPanel = new JPanel();
-        thirdColumnPanel.setLayout(null);
-        thirdColumnPanel.setBackground(new Color(0, 0, 255));
+        // View statistics button
+        JButton viewStatsButton = new JButton("View Statistics");
+        viewStatsButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        viewStatsButton.setBackground(new Color(0, 255, 255));
+        firstColumnInfo.add(viewStatsButton);
 
-        frame.add(titlePanel, BorderLayout.PAGE_START);
-        mainContentPanel.add(firstColumnPanel, BorderLayout.LINE_START);
-        mainContentPanel.add(secondColumnPanel, BorderLayout.CENTER);
-        mainContentPanel.add(thirdColumnPanel, BorderLayout.LINE_END);
-        frame.add(mainContentPanel);
-        frame.setVisible(true); // Runs the GUI to show up
+        // Save library button
+        JButton saveLibraryButton = new JButton("Save Library");
+        saveLibraryButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        saveLibraryButton.setBackground(new Color(255, 0, 255));
+        firstColumnInfo.add(saveLibraryButton);
+
+        // Second column section
+        JPanel secondColumn = new JPanel();
+        JPanel secondColumnInfo = new JPanel();
+        secondColumn
+                .setBorder(BorderFactory.createEmptyBorder(columnPadding, columnPadding, columnPadding, columnPadding));
+        secondColumn.setBackground(new Color(0, 255, 0));
+        secondColumnInfo.setBackground(null);
+        secondColumn.setLayout(new GridLayout(1, 1));
+        secondColumnInfo.setLayout(new GridLayout(3, 1, 0, 75));
+
+        // Remove library button
+        JButton removeLibraryButton = new JButton("Remove Library");
+        removeLibraryButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        removeLibraryButton.setBackground(new Color(0, 255, 255));
+        secondColumnInfo.add(removeLibraryButton);
+
+        // View all books button
+        JButton viewAllBooksButton = new JButton("View All Books");
+        viewAllBooksButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        viewAllBooksButton.setBackground(new Color(255, 0, 255));
+        secondColumnInfo.add(viewAllBooksButton);
+
+        // Load library button
+        JButton loadLibraryButton = new JButton("Load Library");
+        loadLibraryButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        loadLibraryButton.setBackground(new Color(255, 255, 0));
+        secondColumnInfo.add(loadLibraryButton);
+
+        // Third column section
+        JPanel thirdColumn = new JPanel();
+        JPanel thirdColumnInfo = new JPanel();
+        thirdColumn.setBorder(
+                BorderFactory.createEmptyBorder(columnPadding, columnPadding / 2, columnPadding, columnPadding / 2));
+        thirdColumn.setBackground(new Color(0, 0, 255));
+        thirdColumnInfo.setBackground(null);
+        // Set layout for roughly 2:1 ratio
+        thirdColumn.setLayout(new GridLayout(1, 1));
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        thirdColumnInfo.setLayout(gridBagLayout);
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        // Search box area
+        JPanel searchPanel = new JPanel();
+        searchPanel.setBackground(new Color(0, 255, 0));
+        c.weightx = 1.0;
+        c.weighty = 0.6;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.BOTH;
+        thirdColumnInfo.add(searchPanel, c);
+
+        // Spacer panel
+        JPanel spacerPanel = new JPanel();
+        c.weighty = 0.15;
+        c.gridy = 1;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        thirdColumnInfo.add(spacerPanel, c);
+
+        // Exit button
+        JButton exitButton = new JButton("Exit");
+        exitButton.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        exitButton.setBackground(new Color(255, 255, 0));
+        c.weighty = 0.15;
+        c.gridy = 2;
+        c.ipady = -10;
+        c.fill = GridBagConstraints.BOTH;
+        thirdColumnInfo.add(exitButton, c);
+
+        // Set button actions, event listeners, and tooltips
+        newLibraryButton.setActionCommand("new library");
+        removeLibraryButton.setActionCommand("remove library");
+        viewStatsButton.setActionCommand("view stats");
+        viewAllBooksButton.setActionCommand("view all books");
+        saveLibraryButton.setActionCommand("save library");
+        loadLibraryButton.setActionCommand("load library");
+        exitButton.setActionCommand("exit");
+        newLibraryButton.addActionListener(this);
+        viewStatsButton.addActionListener(this);
+        saveLibraryButton.addActionListener(this);
+        removeLibraryButton.addActionListener(this);
+        viewAllBooksButton.addActionListener(this);
+        loadLibraryButton.addActionListener(this);
+        exitButton.addActionListener(this);
+
+        // Add all items to the main GUI frame
+        add(titlePanel, BorderLayout.PAGE_START);
+        firstColumn.add(firstColumnInfo);
+        secondColumn.add(secondColumnInfo);
+        thirdColumn.add(thirdColumnInfo);
+        mainContentPanel.add(firstColumn, BorderLayout.LINE_START);
+        mainContentPanel.add(secondColumn, BorderLayout.CENTER);
+        mainContentPanel.add(thirdColumn, BorderLayout.LINE_END);
+        add(mainContentPanel);
+        setVisible(true); // Runs the GUI to show up
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if ("new library".equals(e.getActionCommand())) {
+            createLibrary();
+        } else if ("remove library".equals(e.getActionCommand())) {
+            removeLibrary();
+        } else if ("view stats".equals(e.getActionCommand())) {
+            viewStats();
+        } else if ("view all books".equals(e.getActionCommand())) {
+            viewAllBooks();
+        } else if ("save library".equals(e.getActionCommand())) {
+            saveLibrary();
+        } else if ("load library".equals(e.getActionCommand())) {
+            bta = loadLibrary();
+        } else if ("exit".equals(e.getActionCommand())) {
+            dispose();
+        }
+    }
+
+    private void createLibrary() {
+        System.out.println("Creating new library...");
+        // bta.addLibrary();
+    }
+
+    private void removeLibrary() {
+        System.out.println("Removing a library...");
+        // bta.removeLibrary();
+    }
+
+    private void viewStats() {
+        System.out.println("Showing all statistics...");
+        // bta.getAverageDuration();
+        // bta.getAveragePageCount();
+        // bta.getAverageWordCount();
+    }
+
+    private void viewAllBooks() {
+        System.out.println("Showing all books' info...");
+        // for (Library lib : bta.getLibraries()) {
+        // lib.getBookCollection();
+        // }
+    }
+
+    private void saveLibrary() {
+        Writer writer = new Writer(filePath);
+
+        try {
+            writer.writeToFile(bta);
+            System.out.println("Saved libraries to '" + writer.getFilePath() + "'!");
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to save libraries to '" + writer.getFilePath() + "'! Please try again.");
+        }
+    }
+
+    private BookTrackerApp loadLibrary() {
+        Reader reader = new Reader(filePath);
+
+        try {
+            bta = reader.readFromFile();
+            System.out.println("Loaded libraries from '" + reader.getFilePath() + "'!");
+        } catch (IOException e) {
+            System.out.println("Unable to load libraries from '" + reader.getFilePath() + "'! Please try again.");
+        }
+
+        return bta;
     }
 }
