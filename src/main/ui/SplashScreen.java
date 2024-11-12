8e0320aa3e733a907eb18677ecf32ba9993c4cb1
Sooraj Sannabhadti <ssannabh@students.cs.ubc.ca>
2024-11-12 11:24:55 -0800

    Create a splash screen class for the splash screen

---
+++ src/main/ui/SplashScreen.java
@@ -0,0 +1,10 @@
+package ui;
+
+import javax.swing.*;
+
+// Displays a splash screen gif before the program starts
+public class SplashScreen extends JFrame {
+    public SplashScreen() {
+        super("Book Tracker App");
+    }
+}
