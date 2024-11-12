75b95da909011647d66aba2b9f00cceb37fa6c19
Sooraj Sannabhadti <ssannabh@students.cs.ubc.ca>
2024-11-12 11:25:35 -0800

    Implement splash screen functionality

--- src/main/ui/SplashScreen.java
+++ src/main/ui/SplashScreen.java
@@ -1,10 +1,36 @@
 package ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 // Displays a splash screen gif before the program starts
 public class SplashScreen extends JFrame {
     public SplashScreen() {
         super("Book Tracker App");
+
+        JLabel splashImage = new JLabel(new ImageIcon("images/splash.gif"));
+        this.add(splashImage);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Set the program to exit when the close button is clicked
+        this.setAlwaysOnTop(true);
+        this.setResizable(false);
+        this.setSize(500, 500); // Set window size
+        this.setLocationRelativeTo(null);
+
+        this.setVisible(true);
+
+        // Taken from Java's javax.swing.Timer tooltip documentation
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                new BookTrackerAppGUI();
+                dispose();
+            }
+        };
+
+        Timer timer = new Timer(4000, taskPerformer);
+        timer.setRepeats(false);
+        timer.start();
     }
 }
